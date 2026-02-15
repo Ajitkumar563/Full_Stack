581ef88310113ff8e283ece1a3906078d0ce4ab5
AjitKumar <Ajitkm8@gmail.com>
2026-02-15 16:47:50 +0530

    Initial Full_stack upload

---
+++ JS/Datatypes_part_1/first.js
@@ -0,0 +1,32 @@
+// declare  the variable 
+
+// let num=10;
+// console.log(num);
+
+// primitive Data type
+// Number, boolean, string, null, undefined, Symbol, bigint  
+
+// Non primitive Data type
+// Array , Object, function 
+
+// str
+let account_balance = 30;
+console.log(typeof account_balance);
+
+let str = "Ajit";
+console.log(str)
+
+// Boolean 
+
+let kisi_ko_block_mara_hai = false;
+console.log(kisi_ko_block_mara_hai);
+
+let account;
+console.log(account)
+
+// Null 
+
+let bal= null;
+console.log(typeof bal);
+
+// bigint
