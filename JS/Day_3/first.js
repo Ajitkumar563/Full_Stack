f596e5aae6d66ce705118a9e15c36c983d82db19
AjitKumar <Ajitkm8@gmail.com>
2026-02-18 20:20:11 +0530

    Added placeholder files

---
+++ JS/Day_3/first.js
@@ -0,0 +1,11 @@
+// Number, string, boolean, null, underfined, Bigint, Symbol
+
+// Non Primitive data types
+// Array, Object, function, date
+
+// let n =10;
+// let n2 =20;
+// let n3 =50;
+
+let arr = [10,20,50];
+console.log(arr);
