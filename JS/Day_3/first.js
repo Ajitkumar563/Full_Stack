82d4f76f89311bc3ac676478376fa8ac1bbe1d3b
AjitKumar <Ajitkm8@gmail.com>
2026-02-18 20:32:15 +0530

    Added placeholder files

--- JS/Day_3/first.js
+++ JS/Day_3/first.js
@@ -6,6 +6,18 @@
 // let n =10;
 // let n2 =20;
 // let n3 =50;
+// array
 
 let arr = [10,20,50,"Ajit","mohit"];
 console.log(typeof arr);
+
+// Object 
+
+let obj ={
+    user_name : "Ajit",
+    account_number : 3222323,
+    balance: 420
+
+}
+ console.log(obj);
+ console.log(typeof obj)
