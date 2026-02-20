437055b9432890f6df115b5c7d9cd741b9ace80c
AjitKumar <Ajitkm8@gmail.com>
2026-02-20 21:13:33 +0530

    Added placeholder files1

--- JS/Day_4/index.js
+++ JS/Day_4/index.js
@@ -1,18 +1,18 @@
 // Comparision operator
 // number to number
 
-let a =4;
-let b= 5;
-console.log(a==b);
+// let a =4;
+// let b= 5;
+// console.log(a==b);
 
 // let A =4;
 // let B= 4;
 // console.log(A==B);
 
 
-let A1 =4;
-let B1= 4;
-console.log(A1>B1);
+// let A1 =4;
+// let B1= 4;
+// console.log(A1>B1);
 
 // let A =5;
 // let B= 6;
@@ -22,9 +22,9 @@ console.log(A1>B1);
 // let B= 6;
 // console.log(A<=B);
 
-let A =5;
-let B= 6;
-console.log(A==B);
+// let A =5;
+// let B= 6;
+// console.log(A==B);
 
 // == both are equal 
 // < less than , > greater than
@@ -34,22 +34,28 @@ console.log(A==B);
 // let str = "10";
 // console.log(num==str)
 
-let num = 10;
-let str = "20";
-console.log(num==str);
+// let num = 10;
+// let str = "20";
+// console.log(num==str);
 // Type conversion hoga string to number
 
-let a1 = 10;
-let str1 = "10";
-console.log(a1<str1);
+// let a1 = 10;
+// let str1 = "10";
+// console.log(a1<str1);
 
 // === type check, then comapare the value
-console.log(a1===str1)
+// console.log(a1===str1)
 
 // let a2 = 30;
 // let a3 = 30;
 // console.log(a2===a3)
 
-let a2 = 30;
-let a3 = 40;
-console.log(a2===a3)
+// let a2 = 30;
+// let a3 = 40;
+// console.log(a2===a3)
+
+// null == undefined // true
+// null === undefined //  false
+
+console.log(null==undefined);
+console.log(null===undefined);
