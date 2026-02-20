b12d66eebc3358bb9fb55d28552b43bdaac9490a
AjitKumar <Ajitkm8@gmail.com>
2026-02-20 20:09:21 +0530

    Added placeholder files1

--- JS/Day_4/index.js
+++ JS/Day_4/index.js
@@ -5,11 +5,23 @@ let a =4;
 let b= 5;
 console.log(a==b);
 
-let A =4;
-let B= 4;
-console.log(A==B);
+// let A =4;
+// let B= 4;
+// console.log(A==B);
 
 
 let A1 =4;
 let B1= 4;
 console.log(A1>B1);
+
+// let A =5;
+// let B= 6;
+// console.log(A>=B);
+
+// let A =5;
+// let B= 6;
+// console.log(A<=B);
+
+let A =5;
+let B= 6;
+console.log(A==B);
