1c158b28b3e835db8cd0eee9c1caef6856236e38
AjitKumar <Ajitkm8@gmail.com>
2026-02-20 20:17:32 +0530

    Added placeholder files1

--- JS/Day_4/index.js
+++ JS/Day_4/index.js
@@ -25,3 +25,15 @@ console.log(A1>B1);
 let A =5;
 let B= 6;
 console.log(A==B);
+
+// == both are equal 
+// < less than , > greater than
+// <= less then equal to, >= greater than equal to
+
+// let num = 10;
+// let str = "10";
+// console.log(num==str)
+
+let num = 10;
+let str = "20";
+console.log(num==str)
