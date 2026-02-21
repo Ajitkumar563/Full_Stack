ab11eefc18992f3391cc99127985d08f6a9602a5
AjitKumar <Ajitkm8@gmail.com>
2026-02-21 22:40:40 +0530

    Added placeholder files

---
+++ JS/Day_5/first.js
@@ -0,0 +1,8 @@
+let a =10;
+let b=a;
+b =30;
+console.log(b);
+console.log(a);
+// Primitive data type vs Non Primitive data type
+// Primitive data type : Immutable
+// NON Primitive data type : mutable
