0f9ff372ec7d3b31723001a4c500ef13d4da389b
AjitKumar <Ajitkm8@gmail.com>
2026-02-21 22:48:35 +0530

    Added placeholder files

--- JS/Day_5/first.js
+++ JS/Day_5/first.js
@@ -6,3 +6,18 @@ console.log(a);
 // Primitive data type vs Non Primitive data type
 // Primitive data type : Immutable
 // NON Primitive data type : mutable
+
+// Object example :
+
+let obj1 ={
+    id:20,
+    naming: "rohit"
+}
+
+let obj2 = obj1;
+// console.log(obj1);
+// console.log(obj2);
+
+obj2.id=30;
+console.log(obj1);
+console.log(obj2);
