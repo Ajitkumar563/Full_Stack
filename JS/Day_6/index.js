669e98543cec65c4b7a8ad124d45497cd22eb617
AjitKumar <Ajitkm8@gmail.com>
2026-02-22 12:02:47 +0530

    Added placeholder files

--- JS/Day_6/index.js
+++ JS/Day_6/index.js
@@ -1,24 +1,36 @@
 // primitive data type 
 
-const num = 10;
-console.log(num)
+// const num = 10;
+// console.log(num)
 
-// Non Primitive datatype
-const obj ={
-    id:10,
-    balance:200
-}
+// // Non Primitive datatype
+// const obj ={
+//     id:10,
+//     balance:200
+// }
 
-obj.id =11;
-console.log(obj);
+// obj.id =11;
+// console.log(obj);
 
-let obj2 ={
-    id:20,
-    money:30
-};
+// let obj2 ={
+//     id:20,
+//     money:30
+// };
 
 // location of obj2 is 800
 // location of obj is 6521
 
-obj =obj2;
+// obj =obj2;
 // error
+
+// string in js
+let str1 = "Hello Coder army";
+let str2 = 'mein toh mst hu';
+let str3 = 'Aur bhai kya haal chaal';
+
+let price =50
+
+console.log(`price of the tomato is ${price}`);
+console.log(str1,str2,str3);
+
+
