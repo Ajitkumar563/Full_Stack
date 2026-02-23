ca8408af1e3c629e6d2b527e8346bbdbcf2d51e5
AjitKumar <Ajitkm8@gmail.com>
2026-02-23 22:55:09 +0530

    Added placeholder files

--- JS/Day_07/index.js
+++ JS/Day_07/index.js
@@ -1,7 +1,11 @@
- let num1 =234;
- let num2 = new Number(234);
- let num3 = new Number(234);
- console.log(num1==num2);
- console.log(num2==num3);
+//  let num1 =234;
+//  let num2 = new Number(234);
+//  let num3 = new Number(234);
+//  console.log(num1==num2);
+//  console.log(num2==num3);
 //  console.log(num2);
 //  console.log(typeof num2);
+
+let num = 231.68;
+console.log(num.toFixed )
+console.log(num.toPrecision)
