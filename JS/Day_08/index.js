ee3db5ce291c61ec284316e49de922bbf43782d9
AjitKumar <Ajitkm8@gmail.com>
2026-02-26 00:00:55 +0530

    Added placeholder files

--- JS/Day_08/index.js
+++ JS/Day_08/index.js
@@ -1,10 +1,14 @@
- const arr = [2,34,1,4,6,"Ajit",true];
- console.log(arr[0]);
- console.log(arr.length);
+const arr = [2,34,1,4,6,"Ajit",true];
+//  console.log(arr[0]);
+//  console.log(arr.length);
 
- // at is latest, negative index le leta hai
- console.log(arr.at(0));
+//  // at is latest, negative index le leta hai
+//  console.log(arr.at(0));
 
- const newarr = arr;
- console.log(newarr==arr);
+//  const newarr = arr;
+//  console.log(newarr==arr);
 
+// push 
+arr.push(30);
+arr.push(50);
+console.log(arr);
