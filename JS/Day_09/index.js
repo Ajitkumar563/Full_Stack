58324ce4349122530f921861c507e879bfa74f38
AjitKumar <Ajitkm8@gmail.com>
2026-02-27 19:50:46 +0530

    Added placeholder files

--- JS/Day_09/index.js
+++ JS/Day_09/index.js
@@ -22,7 +22,16 @@
 // const now = Date.now();
 // console.log(now);
 
-const d = new Date("2022-10-20");
-// year / Month / Date / Hour / Minutes / second / millisecond
-const date = new Date(2024,4,28);
-console.log(date.toDateString());
+// const d = new Date("2022-10-20");
+// // year / Month / Date / Hour / Minutes / second / millisecond
+// const date = new Date(2024,4,28);
+// console.log(date.toDateString());
+
+const d = new Date();
+d.setDate(20);
+d.setFullYear(2021);
+d.setMonth(3);
+console.log(d);
+console.log(typeof d);
+console.log(d.toString);
+
