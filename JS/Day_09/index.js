871447aeb4995808d571572fed5d34b73d507d29
AjitKumar <Ajitkm8@gmail.com>
2026-02-27 19:04:08 +0530

    Added placeholder files

--- JS/Day_09/index.js
+++ JS/Day_09/index.js
@@ -1,4 +1,7 @@
 const d = new Date();
-console.log(d.toDateString());
-console.log(d.toString());
-console.log(d.toISOString());
+
+// console.log(d.toDateString());
+// console.log(d.toString());
+// console.log(d.toISOString());
+
+console.log(d);
