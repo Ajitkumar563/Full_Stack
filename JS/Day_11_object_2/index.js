dabd3fed771e96c893bca88a9c8ce24431b5ebc5
AjitKumar <Ajitkm8@gmail.com>
2026-03-05 07:55:02 +0530

    Added placeholder files

--- JS/Day_11_object_2/index.js
+++ JS/Day_11_object_2/index.js
@@ -1,16 +1,16 @@
-let obj1 = {
-    a:1,
-    b:2
-}
-console.log(obj1)
+// let obj1 = {
+//     a:1,
+//     b:2
+// }
+// console.log(obj1)
 
-let obj2 =obj1;
-// shallow copy 
+// let obj2 =obj1;
+// // shallow copy 
 
-obj2.a =10;
-console.log(obj2,obj1);
+// obj2.a =10;
+// console.log(obj2,obj1);
 
-// deep copy
-let obj3= structuredClone(obj1);
-obj3.a = 20;
-console.log(obj3, obj1);
+// // deep copy
+// let obj3= structuredClone(obj1);
+// obj3.a = 20;
+// console.log(obj3, obj1);
