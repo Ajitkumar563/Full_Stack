6e7e506715debe5644eb1d48be7f61ee1a1e4d8e
AjitKumar <Ajitkm8@gmail.com>
2026-04-03 22:16:28 +0530

    message

--- JS/Day_13/index.js
+++ JS/Day_13/index.js
@@ -1,11 +1,11 @@
 // if -else
 let age =30;
 
-if(age>=18)
-{
-    console.log("Eligible for vote");
-}    
-else
-{
-    console.log("Not Eligible for vote");
-}    
+// if(age>=18)
+// {
+//     console.log("Eligible for vote");
+// }    
+// else
+// {
+//     console.log("Not Eligible for vote");
+// }    
