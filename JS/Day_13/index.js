5cf6133856f355305449ccd1016c9ead1483002f
AjitKumar <Ajitkm8@gmail.com>
2026-04-03 23:42:23 +0530

    message

--- JS/Day_13/index.js
+++ JS/Day_13/index.js
@@ -95,9 +95,9 @@
 // }
 
 // sum of first n number
-let sum =0;
-for(let i=1;i<=10;i++)
-{
-    sum+=1;
-}   
-console.log(sum) 
+// let sum =0;
+// for(let i=1;i<=10;i++)
+// {
+//     sum+=1;
+// }   
+// console.log(sum) 
