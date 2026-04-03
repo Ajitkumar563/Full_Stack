93be6a4a3ae49d9e9bcc9c5775e9a1515e928410
AjitKumar <Ajitkm8@gmail.com>
2026-04-03 22:49:35 +0530

    message

--- JS/Day_13/index.js
+++ JS/Day_13/index.js
@@ -33,4 +33,26 @@
 switch (new Date().getDay())
 {
     case 0:
+        console.log("SUNDAY");
+        break;
+    case 1:
+        console.log("MONDEY");
+        break;
+    case 2:
+        console.log("TUESDAY");
+        break;  
+    case 3:
+        console.log("wEDNESDAY");
+        break; 
+    case 4:
+        console.log("THURSDAY");
+        break; 
+    case 5:
+        console.log("FRIDAY");
+        break; 
+    case 6:
+        console.log("SATURDAY");
+        break; 
+                           
+
 }
