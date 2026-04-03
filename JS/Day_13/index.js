3b9419dc7aaa451b9064643f1f5c5474ef60cb1d
AjitKumar <Ajitkm8@gmail.com>
2026-04-03 23:23:44 +0530

    message

--- JS/Day_13/index.js
+++ JS/Day_13/index.js
@@ -57,32 +57,32 @@
 
 // }
 
-let day ="0"
-switch (day)
-{
-    case 0:
-        console.log("SUNDAY");
-        break;
-    case 1:
-        console.log("MONDEY");
-        break;
-    case 2:
-        console.log("TUESDAY");
-        break;  
-    case 3:
-        console.log("wEDNESDAY");
-        break; 
-    case 4:
-        console.log("THURSDAY");
-        break; 
-    case 5:
-        console.log("FRIDAY");
-        break; 
-    case 6:
-        console.log("SATURDAY");
-        break; 
-    default:
-        console.log("Not a valid day")    
+// let day ="0"
+// switch (day)
+// {
+//     case 0:
+//         console.log("SUNDAY");
+//         break;
+//     case 1:
+//         console.log("MONDEY");
+//         break;
+//     case 2:
+//         console.log("TUESDAY");
+//         break;  
+//     case 3:
+//         console.log("wEDNESDAY");
+//         break; 
+//     case 4:
+//         console.log("THURSDAY");
+//         break; 
+//     case 5:
+//         console.log("FRIDAY");
+//         break; 
+//     case 6:
+//         console.log("SATURDAY");
+//         break; 
+//     default:
+//         console.log("Not a valid day")    
                            
 
-}
+// }
