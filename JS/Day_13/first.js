f854f2111c7d592efc690d04785d01c8ab8ade9d
AjitKumar <Ajitkm8@gmail.com>
2026-04-04 09:52:16 +0530

    message

--- JS/Day_13/first.js
+++ JS/Day_13/first.js
@@ -2,19 +2,19 @@
 
 // Global scope, local scope(functional scope), black scope
 
-let a = 10;
-var b = 20;
-const c = 30;
+// let a = 10;
+// var b = 20;
+// const c = 30;
 
-function greet(){
-    console.log(a);
-    console.log(b);
-    console.log(c);
-    console.log("Ajit")
-};
+// function greet(){
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+//     console.log("Ajit")
+// };
 
-greet();
-console.log(a)
+// greet();
+// console.log(b);
 
 
 
