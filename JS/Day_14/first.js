8316493f2f02b074ef04d6c6817d1d17b01d0879
AjitKumar <Ajitkm8@gmail.com>
2026-04-14 11:00:11 +0530

    message

--- JS/Day_14/first.js
+++ JS/Day_14/first.js
@@ -1,10 +1,10 @@
-let obj = {};
+// let obj = {};
 
-obj.name = "Ajit";
-// key value writable enumerable configurable
+// obj.name = "Ajit";
+// // key value writable enumerable configurable
 
-console.log(Object.getOwnPropertyDescriptor(obj,'name'))
-// writable = false, value ko kya mein change kar sakta hu
+// console.log(Object.getOwnPropertyDescriptor(obj,'name'))
+// // writable = false, value ko kya mein change kar sakta hu
 
-obj.name = "mohit"
-//configurable = false
+// obj.name = "mohit"
+// //configurable = false
