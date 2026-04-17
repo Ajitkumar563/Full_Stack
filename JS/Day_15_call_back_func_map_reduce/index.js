fedf1a473a544c7b7f6d3258c736ff1ad131f6e6
AjitKumar <Ajitkm8@gmail.com>
2026-04-17 20:14:03 +0530

    message

--- JS/Day_15_call_back_func_map_reduce/index.js
+++ JS/Day_15_call_back_func_map_reduce/index.js
@@ -1,14 +1,14 @@
-let user ={
-    name : "Ajit",
-    age : 30
-}
+// let user ={
+//     name : "Ajit",
+//     age : 30
+// }
 
-console.log(Object.getOwnPropertyDescriptor(user,"name")) 
-Object.defineProperty(user,'name',{
-    writable:false,
-});
+// console.log(Object.getOwnPropertyDescriptor(user,"name")) 
+// Object.defineProperty(user,'name',{
+//     writable:false,
+// });
 
-user.name = "ram";
-// hacking se bachne ke liye humne ye sb kiya hai 
+// user.name = "ram";
+// // hacking se bachne ke liye humne ye sb kiya hai 
 
-console.log(Object.getOwnPropertyDescriptor(user, "name"))
+// console.log(Object.getOwnPropertyDescriptor(user, "name"))
