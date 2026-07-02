f2814fe2b39c282d688df6ce9bca048ca38de04e
AjitKumar <Ajitkm8@gmail.com>
2026-07-03 00:08:45 +0530

    Initial commit

--- JS/Day_16/index.js
+++ JS/Day_16/index.js
@@ -1,6 +1,6 @@
 // reduce 
 
-const arr = [10,20,30,40,50]
+// const arr = [10,20,30,40,50]
 
 // const result = arr.reduce(callback function, initialization)
 // const result = arr.reduce((acc,curr)=>{
@@ -9,3 +9,10 @@ const arr = [10,20,30,40,50]
 // },0)
 
 // console.log(result);
+
+let arr = ["arange","apple","banana", "orange", "apple", "banana", "orange", "grape"]
+
+// final result ek object ke form
+const result =arr.reduce((accepts,curr)=>{
+
+}{})
